9ee15a1e9e99bad6e3010df0ad246b8aecd51f1e
pospi <pospi@spadgos.com>
2019-01-09 13:01:41 +1000

    stub out root gQL queries and start stubbing types, pulled from Valuenetwork API

---
+++ src/graphql-adapter/src/index.ts
@@ -0,0 +1,85 @@
+/**
+ * HoloREA GraphQL adapter for standard Holochain zome REST APIs
+ *
+ * @package: HoloREA
+ * @author:  pospi <pospi@spadgos.com>
+ * @since:   2019-01-03
+ */
+
+import {
+  GraphQLInputType,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLArgumentConfig,
+  GraphQLFieldConfigArgumentMap,
+} from 'graphql'
+
+import {
+  GraphQLArgDef,
+  GraphQLArgumentConfigWithIndex,
+} from './queries'
+import * as queries from './queries'
+import * as types from './types'
+
+type QueriesType = typeof queries
+type QueryId = keyof QueriesType
+
+interface QueryDef {
+  type: GraphQLObjectType,
+  resolve: (...values: any[]) => any,
+  args?: GraphQLFieldConfigArgumentMap
+}
+
+interface GraphQLFieldsDef {
+  [fieldName: string]: QueryDef
+}
+
+interface ArgsDef {
+  [arg: string]: GraphQLArgDef,
+}
+
+function isRawArgumentConfig(object: GraphQLArgDef): object is GraphQLArgumentConfigWithIndex {
+  return 'type' in object
+}
+
+function isSimpleArgumentConfig(object: GraphQLArgDef): object is GraphQLInputType {
+  return !('type' in object)
+}
+
+function queryFieldsReducer(f: GraphQLFieldsDef, query: QueryId) {
+  const { resultType, args, resolve } = queries[query];
+  // assign field / return value type and resolution logic
+  f[query] = {
+    type: resultType,
+    resolve: resolve,
+  }
+  if (args) {
+    // inflate arg values to reduce verbosity in declaration
+    f[query]['args'] = Object.keys(args).reduce((a: ArgsDef, arg: string) => {
+      const argData = args[arg]
+      if (isRawArgumentConfig(argData)) {
+        const typedArg: GraphQLArgumentConfig = argData
+        a[arg] = typedArg;  // may also have `defaultValue` and `description`
+      } else if (isSimpleArgumentConfig(argData)) {
+        const typedArg: GraphQLArgumentConfig = { type: argData }
+        a[arg] = typedArg  // simple type, inflate to `type` subkey
+      }
+      return a
+    }, {})
+  }
+  return f
+}
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'The root of all REA queries',
+  fields: () => Object.keys(queries)
+    .map((query: string): QueryId => query as QueryId)
+    .reduce(queryFieldsReducer, {})
+});
+
+export default new GraphQLSchema({
+  query: QueryType,
+});
---
+++ src/graphql-adapter/src/types.ts
@@ -0,0 +1,314 @@
+/**
+ * type schemas for GraphQL query layer
+ *
+ * @see https://github.com/FreedomCoop/valuenetwork/tree/71b0868/valuenetwork/api/types
+ *
+ * @package: HoloREA
+ * @author:  pospi <pospi@spadgos.com>
+ * @since:   2019-01-03
+ */
+
+import { __, apply } from 'ramda'
+import fecha from 'fecha'
+
+import {
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLEnumType,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLID
+} from 'graphql'
+
+// import {
+//   VfObject, QuantityValue as coreQV, Hash, QVlike, notError, CrudResponse,
+//   PhysicalLocation, HoloThing, entryOf, hashOf, deepAssign, Initializer, Fixture, reader
+// } from "../../HoloREA/dna/common/common";
+
+// base types
+
+const isoDateRegex = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d[+-]\d\d:\d\d$/
+const parseDate = apply(fecha.parse, __, 'YYYY-MM-DDTHH:mm:ssZZ')
+
+// :TODO: check date field compat with ValueNetwork API
+
+export const StringDate = new GraphQLScalarType({
+  name: 'Date (ISO8601)',
+  serialize: parseDate,
+  parseValue: parseDate,
+  parseLiteral(ast) {
+    if (ast.kind === Kind.STRING && ast.value.match(isoDateRegex)) {
+      return parseDate(ast.value);
+    }
+    return null;
+  }
+})
+
+// predefined (hardcoded) taxonomies
+
+export const Action = new GraphQLEnumType({
+  name: "Action",
+  values: {
+    NONE: { value: 0 },
+    ACCEPT: { value: 1 },
+    ADJUST: { value: 2 },
+    CITE: { value: 3 },
+    CONSUME: { value: 4 },
+    GIVE: { value: 5 },
+    IMPROVE: { value: 6 },
+    PRODUCE: { value: 7 },
+    TAKE: { value: 8 },
+    USE: { value: 9 },
+    WORK: { value: 10 },
+  }
+})
+
+export const EconomicResourceProcessCategory = new GraphQLEnumType({
+  name: 'Economic resource process categories',
+  values: {
+    NONE: { value: 0 },
+    CITED: { value: 1 },
+    CONSUMED: { value: 2 },
+    PRODUCED: { value: 3 },
+    USED: { value: 4 },
+  }
+})
+
+// config / system layer
+
+export const Unit = new GraphQLObjectType({
+  name: 'Unit',
+  description: 'A measurement unit for quantifying resources',
+  fields: () => ({
+    id: { type: GraphQLID },
+    // :TODO: i18n
+    name: { type: GraphQLString },
+    symbol: { type: GraphQLString },
+  })
+})
+
+export const QuantityValue = new GraphQLObjectType({
+  name: 'Quantity value',
+  description: 'Some measured quantity, recorded against a particular measurement unit',
+  fields: () => ({
+    numericValue: { type: GraphQLID },
+    unit: { type: Unit },
+  })
+})
+
+export const NotificationType = new GraphQLObjectType({
+  name: 'Notification type',
+  description: 'A definition for a notification type, which can be configured to fire in response to various system actions.',
+  fields: () => ({
+    id: { type: GraphQLID },
+    // :TODO: i18n
+    label: { type: GraphQLString },
+    description: { type: GraphQLString },
+    display: { type: GraphQLString },
+  })
+})
+
+export const ResourceClassification = new GraphQLObjectType({
+})
+
+export const ProcessClassification = new GraphQLObjectType({
+})
+
+export const AgentResourceClassification = new GraphQLObjectType({
+})
+
+export const OrganizationClassification = new GraphQLObjectType({
+})
+
+export const AgentRelationshipRole = new GraphQLObjectType({
+})
+
+export const OrganizationType = new GraphQLObjectType({
+})
+
+export const Facet = new GraphQLObjectType({
+})
+
+export const Place = new GraphQLObjectType({
+})
+
+// planning layer
+
+export const Plan = new GraphQLObjectType({
+})
+
+export const Commitment = new GraphQLObjectType({
+})
+
+export const ExchangeAgreement = new GraphQLObjectType({
+})
+
+// actuals layer
+
+export const Process = new GraphQLObjectType({
+})
+
+export const EconomicResource = new GraphQLObjectType({
+})
+
+export const EconomicEvent = new GraphQLObjectType({
+})
+
+export const Exchange = new GraphQLObjectType({
+})
+
+export const Transfer = new GraphQLObjectType({
+})
+
+export const Validation = new GraphQLObjectType({
+})
+
+// agents
+
+export const AgentRelationship = new GraphQLObjectType({
+})
+
+export const Agent = new GraphQLObjectType({
+  name: 'Agent',
+  description: 'An agent in an REA-based economic network',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    type: { type: GraphQLString },
+    image: { type: GraphQLString },
+    note: { type: GraphQLString },
+    primaryLocation: { type: GraphQLString },
+    primaryPhone: { type: GraphQLString },
+    email: { type: GraphQLString },
+    ownedEconomicResources
+    : { type: new GraphQLList(EconomicResource), resolve: (agent, args) => {
+      //(category: EconomicResourceCategoryresourceClassificationId: Intpage: Int)
+
+    } },
+    searchOwnedInventoryResources
+    : { type: new GraphQLList(EconomicResource), resolve: (agent, args) => {
+      //(searchString: String)
+
+    } },
+    agentProcesses
+    : { type: new GraphQLList(Process), resolve: (agent, args) => {
+      //(isFinished: Boolean)
+
+    } },
+    searchAgentProcesses
+    : { type: new GraphQLList(Process), resolve: (agent, args) => {
+      //(searchString: StringisFinished: Boolean)
+
+    } },
+    agentPlans
+    : { type: new GraphQLList(Plan), resolve: (agent, args) => {
+      //(isFinished: Booleanyear: Intmonth: Int)
+
+    } },
+    searchAgentPlans
+    : { type: new GraphQLList(Plan), resolve: (agent, args) => {
+      //(searchString: StringisFinished: Boolean)
+
+    } },
+    agentEconomicEvents
+    : { type: new GraphQLList(EconomicEvent), resolve: (agent, args) => {
+      //(latestNumberOfDays: IntrequestDistribution: Booleanaction: Stringyear: Intmonth: Int)
+
+    } },
+    agentCommitments
+    : { type: new GraphQLList(Commitment), resolve: (agent, args) => {
+      //(latestNumberOfDays: Int)
+
+    } },
+    searchAgentCommitments
+    : { type: new GraphQLList(Commitment), resolve: (agent, args) => {
+      //(searchString: StringisFinished: Boolean)
+
+    } },
+    agentRelationships
+    : { type: new GraphQLList(AgentRelationship), resolve: (agent, args) => {
+      //(roleId: Intcategory: AgentRelationshipCategory)
+
+    } },
+    agentRelationshipsAsSubject
+    : { type: new GraphQLList(AgentRelationship), resolve: (agent, args) => {
+      //(roleId: Intcategory: AgentRelationshipCategory)
+
+    } },
+    agentRelationshipsAsObject
+    : { type: new GraphQLList(AgentRelationship), resolve: (agent, args) => {
+      //(roleId: Intcategory: AgentRelationshipCategory)
+
+    } },
+    agentRoles
+    : { type: new GraphQLList(AgentRelationshipRole), resolve: agent => {
+
+    } },
+    agentRecipes
+    : { type: new GraphQLList(ResourceClassification), resolve: agent => {
+
+    } },
+    // :TODO: rethink account addresses & standardise FreedomCoop additions in valuenetwork
+    // faircoinAddress: { type: GraphQLString },
+    agentNotificationSettings
+    : { type: new GraphQLList(NotificationSetting), resolve: agent => {
+
+    } },
+    memberRelationships
+    : { type: new GraphQLList(AgentRelationship), resolve: agent => {
+
+    } },
+    agentSkills
+    : { type: new GraphQLList(ResourceClassification), resolve: agent => {
+
+    } },
+    agentSkillRelationships
+    : { type: new GraphQLList(AgentResourceClassification), resolve: agent => {
+
+    } },
+    commitmentsMatchingSkills
+    : { type: new GraphQLList(Commitment), resolve: agent => {
+
+    } },
+    validatedEventsCount
+    : { type: GraphQLInt, resolve: (agent, args) => {
+      //(month: Int, year: Int) :TODO: why is this backwards?
+
+    } },
+    eventsCount
+    : { type: GraphQLInt, resolve: (agent, args) => {
+      //(year: Int, month: Int)
+
+    } },
+    eventHoursCount
+    : { type: GraphQLInt, resolve: (agent, args) => {
+      //(year: Int, month: Int)
+
+    } },
+    eventPeopleCount
+    : { type: GraphQLInt, resolve: (agent, args) => {
+      //(year: Int, month: Int)
+
+    } },
+  }),
+});
+
+export const Organization = new GraphQLObjectType({
+})
+
+export const Person = new GraphQLObjectType({
+})
+
+export const NotificationSetting = new GraphQLObjectType({
+  name: 'Notification setting',
+  description: 'Controls user preferences as to whether to enable notifications for a particular subset of REA system functionality.',
+  fields: () => ({
+    id: { type: GraphQLID },
+    send: { type: GraphQLBoolean },
+    agent: { type: Agent, resolve: (setting, args) => {
+    } },
+    notificationType: NotificationType,
+  })
+})

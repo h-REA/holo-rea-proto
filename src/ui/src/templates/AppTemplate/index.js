7ba5e3fbc531ea593a76793b2dc36c4093919396
pospi <pospi@spadgos.com>
2019-01-16 18:24:54 +1000

    wired up GraphQL browser to demo UI as browser extension had some caching issues

--- src/ui/src/templates/AppTemplate/index.js
+++ src/ui/src/templates/AppTemplate/index.js
@@ -1,21 +1,32 @@
 import * as React from 'react'
-import styled from 'styled-components'
-import Agent from '../../pages/agent'
+// import styled from 'styled-components'
+// import Agent from '../../pages/agent'
 
-const Surface = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  width: 1010px;
-  margin: 0 auto;
-`
+// :TODO: might want to keep this in as a top-level page for demoing, once routing is ready to do that
+import schema from '@holorea/graphql-adapter/src'
+import GraphiQL from 'graphiql'
+import { graphQLFetcher } from '../../store'
+
+// const Surface = styled.div`
+//   height: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   width: 1010px;
+//   margin: 0 auto;
+// `
 
 const AppTemplate = props => {
   return (
-    <Surface>
-      <Agent />
-    </Surface>
+    <GraphiQL
+      fetcher={graphQLFetcher}
+      schema={schema}
+      editorTheme='solarized light' />
   )
+  // return (
+  //   <Surface>
+  //     <Agent />
+  //   </Surface>
+  // )
 }
 
 export default AppTemplate

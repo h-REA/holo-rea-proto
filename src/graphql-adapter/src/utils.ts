a40f1abb1736c689a3a345cd9beae5df4c4d61dd
pospi <pospi@spadgos.com>
2019-01-19 13:40:32 +1000

    more readable with input types on first line

--- src/graphql-adapter/src/utils.ts
+++ src/graphql-adapter/src/utils.ts
@@ -12,15 +12,15 @@ import { DHTResponse, Hash } from '@holorea/zome-api-wrapper'
 type DHTReadFn<T> = (which: Hash<T>[]) => Promise<DHTResponse<T>[]>
 
 // abstractions that these helpers provide
-export const readSingleEntry =
-  <T extends {}>(reader: DHTReadFn<T>) =>
+export const readSingleEntry = <T extends {}>
+  (reader: DHTReadFn<T>) =>
     async (id: string): Promise<DHTResponse<T>> => {
       const records = await reader([id])
       return records[0]
     }
 
-export const resolveSingleEntry =
-  <T extends {}, R extends DHTResponse<any>>(reader: DHTReadFn<T>) =>
+export const resolveSingleEntry = <T extends {}, R extends DHTResponse<any>>
+  (reader: DHTReadFn<T>) =>
     (refField: string) =>
       async (inputObj: R): Promise<DHTResponse<T>> => {
         return readSingleEntry(reader)(inputObj.entry[refField])

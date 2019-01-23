84dac79e41c4c6e9218d9b417821385484a3b8b9
pospi <pospi@spadgos.com>
2019-01-23 13:07:08 +1000

    expose readTransferClasses in zome API

--- src/zome-api-wrapper/zomes.js
+++ src/zome-api-wrapper/zomes.js
@@ -98,7 +98,7 @@ export const resources = new Zome(`resources`, [
 
 export const events = new Zome(`events`, [
   `createEvent`, `createAction`, `createTransfer`, `createTransferClass`,
-  `getFixtures`, `readActions`, 'readProcessClasses', 'readEvents',
+  `getFixtures`, `readActions`, 'readProcessClasses', 'readTransferClasses', 'readEvents',
   `traceEvents`, `trackEvents`, `traceTransfers`, `trackTransfers`,
   `eventSubtotals`, `eventsStartedAfter`, `eventsStartedBefore`, `eventsEndedAfter`,
   `eventsEndedBefore`, `sortEvents`, `resourceCreationEvent`

b83d06edce097a4fb09b44d6e51dd6694724e0e3
pospi <pospi@spadgos.com>
2019-01-25 14:24:31 +1000

    add routing to app to split GraphQL browser into own page

--- src/ui/src/templates/AppTemplate/index.js
+++ src/ui/src/templates/AppTemplate/index.js
@@ -1,32 +1,43 @@
-import * as React from 'react'
-// import styled from 'styled-components'
-// import Agent from '../../pages/agent'
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import Agent from '../../pages/agent'
 
 // :TODO: might want to keep this in as a top-level page for demoing, once routing is ready to do that
 import schema from '@holorea/graphql-adapter/src'
 import GraphiQL from 'graphiql'
 import { graphQLFetcher } from '../../store'
 
-// const Surface = styled.div`
-//   height: 100%;
-//   display: flex;
-//   flex-direction: column;
-//   width: 1010px;
-//   margin: 0 auto;
-// `
+const Surface = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 1010px;
+  margin: 0 auto;
+`
+
+const DemoUI = () => (
+  <Surface>
+    <Agent />
+  </Surface>
+)
+
+const QueryBrowser = () => (
+  <GraphiQL
+    fetcher={graphQLFetcher}
+    schema={schema}
+    editorTheme='solarized light' />
+)
 
 const AppTemplate = props => {
   return (
-    <GraphiQL
-      fetcher={graphQLFetcher}
-      schema={schema}
-      editorTheme='solarized light' />
+    <Router>
+      <Fragment>
+        <Route path='/' exact component={DemoUI} />
+        <Route path='/graphql' component={QueryBrowser} />
+      </Fragment>
+    </Router>
   )
-  // return (
-  //   <Surface>
-  //     <Agent />
-  //   </Surface>
-  // )
 }
 
 export default AppTemplate
